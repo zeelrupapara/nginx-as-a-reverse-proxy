1fbc617ef99b028dd3732e4058938e9c367c36ed
Jeel Rupapara <zeelrupapara@gmail.com>
2022-12-21 10:06:03 +0000

    feat: add cors in api

--- api/main.go
+++ api/main.go
@@ -4,10 +4,13 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
--- web/components/TestApi.vue
+++ web/components/TestApi.vue
@@ -1,29 +1,32 @@
 <template>
-    <div style="text-align:center; margin-top:20rem;">
-        <h1>This is Res :</h1>
-        {{ api }}
-    </div>
+  <div style="text-align: center; margin-top: 20rem">
+    <h1>This is Res :</h1>
+    {{ api }}
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            res:null
-        }
+  data() {
+    return {
+      res: null,
+    };
+  },
+  computed: {
+    api() {
+      return this.res;
     },
-    computed: {
-        api(){
-            return this.res
-        }
-    },
-    async created() {
-        await this.$axios.get("http://api:8000/").then((res) => {   
-            console.log(res);
-            this.res = res
-      }).catch((err) => {
-        this.res = err
+  },
+  async created() {
+    await this.$axios
+      .get("http://api:8000/")
+      .then((res) => {
+        console.log(res);
+        this.res = res;
+      })
+      .catch((err) => {
+        this.res = err;
       });
-    },
-}
+  },
+};
 </script>
